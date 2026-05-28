f04172284695bd6ffa6fc51d2e7784a79c5d0573
Sushanth P <sushanthp.calicut@gmail.com>
2026-05-28 14:58:28 +0530

    Update: Sitemap changes

--- next.config.js
+++ next.config.js
@@ -2,13 +2,6 @@ const nextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "**" }],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/sitemap.xml',
-        destination: '/api/sitemap',
-      },
-    ]
-  },
+  
 }
 module.exports = nextConfig
