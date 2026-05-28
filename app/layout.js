5a06885decfc131947479124445f6a4e825fd4c1
Sushanth P <sushanthp.calicut@gmail.com>
2026-05-28 23:10:04 +0530

    Update: Updated favicon

--- app/layout.js
+++ app/layout.js
@@ -98,7 +98,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.png" sizes="any" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icon.png" />
         <link rel="apple-touch-icon" href="/apple-icon.png" />
       </head>
